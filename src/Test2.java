63e1201b97de3ad78bb00a3ef5403963ce780f67
sunil9561 <sknaidu.m@gmail.com>
2024-04-17 13:48:26 -0600

    committing to test2 Branchx

---
+++ src/Test2.java
@@ -0,0 +1,18 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String hungry="no";
+		
+		if (hungry == "yes") {
+			System.out.println("Test 2 eat something....");
+		}else{
+			System.out.println("Test 2 Do not work....");
+		}
+		
+	}
+
+}
