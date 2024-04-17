383ccf2167e65d77a1d18130f364644aef10708c
sunil9561 <sknaidu.m@gmail.com>
2024-04-17 15:40:23 -0600

    added new file and removed unnessary files

---
+++ src/Test4.java
@@ -0,0 +1,18 @@
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String hungry="yes";
+		
+		if (hungry == "yes") {
+			System.out.println("4 eat something....");
+		}else{
+			System.out.println("4 Dont work....");
+		}
+		
+	}
+
+}
