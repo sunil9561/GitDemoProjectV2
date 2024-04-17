63184e7913ca464ae42a560e5fabe558e2cdd2a5
sunil9561 <sknaidu.m@gmail.com>
2024-04-17 16:18:18 -0600

    added new file

---
+++ src/Test5.java
@@ -0,0 +1,18 @@
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String hungry="yes";
+		
+		if (hungry == "no") {
+			System.out.println("5 eat something....");
+		}else{
+			System.out.println("5 Dont work....");
+		}
+		
+	}
+
+}
